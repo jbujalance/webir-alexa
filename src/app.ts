6b16779823cf74950ae6c170f603e0f6a8d0d2a1
Jose Bujalance <joseanbu23@gmail.com>
2020-05-16 11:54:44 +0200

    Populate directive dispatcher in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import { DirectiveHandlerDispatcher } from "./handlers/directive-dispatcher";
+import { PowerControllerHandler } from "./handlers/power-controller-handler";
+import { WebIrClient } from "./webir-client";
 import { DiscoveryHandler } from "./handlers/discovery-handler";
 
-const dispatcher = new DirectiveHandlerDispatcher(new DiscoveryHandler(), []);
+const webIrClient = new WebIrClient();
+const dispatcher = new DirectiveHandlerDispatcher(new DiscoveryHandler(), [new PowerControllerHandler(webIrClient)]);
 
 // Entry point to the AWS Lambda
 export const handler = dispatcher.handle.bind(dispatcher);
